f20d83907b77da43232b4dadb398c9e404689d7a
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-22 22:23:01 +0100

    added .env and .gitignore file, built contract signed and begin working with it

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,10 +1,14 @@
+from eth_utils import address
 from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
 
 
 install_solc("0.6.0")
@@ -38,13 +42,35 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # connect to ganache
 w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:7545"))
-chain_id = 5777
-my_address = w3.toChecksumAddress("0xD6BD495CA11BF0439340FA6C22225BFC52282281")
-private_key = "0xa197bd3053d668edf360c0f69e784e51d5747db932302f66da39faffaaa58729"
-
+chain_id = 1337
+my_address = w3.toChecksumAddress("0xbb825B4Dc21c164d8bFbE1E82fcD1D073d34601D")
+private_key = os.getenv("PRIVATE_KEY")
 
 # Create contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 # Get latest test transaction
 nonce = w3.eth.getTransactionCount(my_address)
-print(nonce)
+# 1. Build tx
+# 2. Sign tx
+# 3. Send tx
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "gasPrice": w3.eth.gas_price,
+        "nonce": nonce,
+    }
+)
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+# Sending signed txn
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+
+# Working with contract - need
+# 1. Address
+# 2. ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Call (blue in remix) -> Simulates the call and getting a return value
+# Transact (orange in remix) -> Makes a state change
+print(simple_storage.functions.retrieve().call())
