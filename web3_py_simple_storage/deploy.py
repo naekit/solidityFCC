e5f00eb97c5ecc36934f1c12dcf98228cea58bfd
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-22 20:34:07 +0100

    create contract in python, get latest nonce of transaction, and change address to Checksum

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -39,5 +39,12 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # connect to ganache
 w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:7545"))
 chain_id = 5777
-my_address = "0xD6BD495CA11BF0439340FA6C22225BFC52282281"
+my_address = w3.toChecksumAddress("0xD6BD495CA11BF0439340FA6C22225BFC52282281")
 private_key = "0xa197bd3053d668edf360c0f69e784e51d5747db932302f66da39faffaaa58729"
+
+
+# Create contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get latest test transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
