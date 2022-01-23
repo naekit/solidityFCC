8860f42398a0f55f1f9cea3aa6ffc0dac8960ac5
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-23 23:30:04 +0100

    .deploy SimpleStorage "from": account by importing contract into .py file

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,14 +1,20 @@
-from brownie import accounts, config
+from brownie import accounts, config, SimpleStorage
 import os
 
 
 def deploy_simple_storage():
-    # account = accounts[0] <-- to add from local ganach-cli chain
-    # print(account)
+    # V-V-V to add from local ganach-cli chain
+    account = accounts[0]
+    # this step of deploying is much quicker than web3.py
+    # b4 we had to get ABI, nonce, create contract, create transaction, sign, and send
+    # brownie knows to Transact or Call
+    SimpleStorage.deploy({"from": account})
+    # V-V-V add account from brownie encrypted metamask account we put the private key in for
     # account = accounts.load("fccsolidity")
     # print(account)
-    account = accounts.add(config["wallets"]["from_key"])
-    print(account)
+    # V-V-V add metamask account from local .env file configured in .yaml file
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
 
 
 def main():
