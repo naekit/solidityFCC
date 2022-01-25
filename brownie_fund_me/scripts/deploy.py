715413e2cf951e7cea7f449d413befeeb5554339
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-25 18:34:56 +0100

    built pricefeed address into constructor, .yaml, and deploy.py files based on which network we use

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,10 +1,22 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+    # Pass the price feed address to our fundMe contract
+
+    # if we are on a persistent network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=True,
+    )
     print(f"Contract deployed to {fund_me.address}")
 
 
