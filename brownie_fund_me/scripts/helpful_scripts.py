9b11b223904a4c66e8b36e03ab38f0c5ea24325f
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-25 19:17:12 +0100

    make it exact hardcoded values instead of web3 converting in deploy_mocks

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -18,7 +18,5 @@ def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(
-            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print("Mocks Deployed!")
