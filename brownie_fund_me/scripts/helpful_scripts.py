fadf8e1e3d9dd96f2c8234e2e4e8dc6d7f95159b
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-26 13:28:26 +0100

    create mainnet-fork-dev

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -3,11 +3,15 @@ from web3 import Web3
 
 DECIMALS = 8
 STARTING_PRICE = 200000000000
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
