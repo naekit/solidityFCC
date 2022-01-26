846d230c6ef1a6ab98bed0d2b42d51c63f45d73b
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-26 12:16:50 +0100

    same

--- brownie_fund_me/tests/test_fund_me.py
+++ brownie_fund_me/tests/test_fund_me.py
@@ -22,6 +22,5 @@ def test_only_owner_can_withdraw():
         pytest.skip("only for local testing")
     fund_me = deploy_fund_me()
     bad_actor = accounts.add()
-    fund_me.withdraw({"from": bad_actor})
     with pytest.raises(exceptions.VirtualMachineError):
         fund_me.withdraw({"from": bad_actor})
